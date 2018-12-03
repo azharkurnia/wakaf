a84a4f30e46468304f81ecabc26c2da6e464846f
Azhar Kurnia <azharkurnia19@gmail.com>
2018-12-03 15:43:58 +0700

    deploy ci cd heroku

--- wakafproject/settings.py
+++ wakafproject/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PRODUCTION = os.environ.get('DATABASE_URL') != None
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,6 +86,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#If Using Heroku Environemnt, then Use Database Setting on Heroku
+if PRODUCTION:
+    DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
