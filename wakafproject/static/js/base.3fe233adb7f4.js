261046daa7f756a1d8387937f5046ac7a1e92921
Azhar Kurnia <azharkurnia19@gmail.com>
2019-01-02 01:09:49 +0700

    deploy

---
+++ wakafproject/static/js/base.3fe233adb7f4.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+            // checks if window is scrolled more than 500px, adds/removes solid class
+            if($(this).scrollTop() > 50) {
+                $('.navbar').addClass('solid');
+            } else {
+                $('.navbar').removeClass('solid');
+            }
+        });
+
+        var fileName = location.href.split("/").slice(-1);
+    });
