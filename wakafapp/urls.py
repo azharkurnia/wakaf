87e32c0568eefa05e800cb2bce92b2706311b411
Kemas Khaidar Ali <kemask69@gmail.com>
2019-01-02 14:44:17 +0700

    add aboutus

--- wakafapp/urls.py
+++ wakafapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'program/$', program, name='program'),
     url(r'volunteer/$', volunteer, name='volunteer'),
     url(r'article/$', article, name='article'),
+    url(r'aboutUs/$', aboutUs, name='aboutUs'),
     url(r'^add_donatur/', add_donatur, name='add_donatur'),
     url(r'coba/$', coba, name='coba'),
 ]
